174fe16adad1d51088402b2f6f9001917b551524
Nick_Latinette <nicholas.latinette@gmail.com>
2020-12-30 22:41:52 -0600

    added some basic paragraph for info about mmai, will update and mess with layout later

--- src/components/AboutFolder/MMAIinfo.vue
+++ src/components/AboutFolder/MMAIinfo.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h2>This is the MMAI info boi</h2>
+    <h2>MegaMinerAI Info</h2>
+    <p>
+      MegaMinerAI is a 24-hour artificial intelligence (AI) programming
+      competition hosted each semester by ACM-Game.
+    </p>
+    <p>
+      Competitors must use their 24-hours to develop a program to play a novel
+      game developed by ACM-Game’s
+    </p>
+    <p>
+      development team. The details of the game are kept secret until the day of
+      competition, so competitors must
+    </p>
+    <p>demonstrate their wits and coding skills to develop the best AI!</p>
+
+    <p>
+      Programs are pitted against one another in ACM-Game’s Arena, where
+      thousands of games are played in the cloud.
+    </p>
+    <p>
+      Game results are available to users, so that they may further debug their
+      code and refine their strategy. After the
+    </p>
+    <p>
+      competition ends, a triple-elimination is run to determine the winners.
+      The top three student teams are awarded
+    </p>
+    <p>prize money and crowned as the victors of MegaMinerAI.</p>
   </div>
 </template>
 
@@ -12,4 +39,4 @@ export default Vue.extend({
 });
 </script>
 
-<style></style>
+<style scoped></style>
