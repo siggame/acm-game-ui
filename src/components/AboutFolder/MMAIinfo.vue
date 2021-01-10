0f8f9ee409cc9c1f7f610a8d63e3f5b5c946dd0e
Nick_Latinette <nicholas.latinette@gmail.com>
2021-01-09 22:16:31 -0600

    testing some dividers

--- src/components/AboutFolder/MMAIinfo.vue
+++ src/components/AboutFolder/MMAIinfo.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <h2 style="padding: 5px;">MegaMinerAI Info</h2>
+    <h2 style="padding: 5px; text-align:left;">MegaMinerAI Info</h2>
+    <hr class="divide" />
     <p style="text-align: left; padding: 7px;">
       <img
         style="float: right; margin: 0px 0px 20px 20px;"
-        src="../../assets/logo.png"
-        width="120"
+        src="../../assets/mmaiphoto.jpg"
+        width="320"
       />
       MegaMinerAI is a 24-hour artificial intelligence (AI) programming
       competition hosted each semester by ACM-Game. <br />
@@ -14,14 +15,16 @@
       development team. The details of the game are kept secret until the day of
       competition, so competitors must <br />
       demonstrate their wits and coding skills to develop the best AI! Programs
-      are pitted against one another in ACM-Game’s Arena, where <br />
-      thousands of games are played in the cloud. Game results are available to
-      users, so that they may further debug their <br />
+      are pitted against one another in ACM-Game’s Arena, where thousands of
+      games are played in the cloud. Game results are available to users, so
+      that they may further debug their <br />
       code and refine their strategy. After the competition ends, a
       triple-elimination is run to determine the winners. <br />
       The top three student teams are awarded prize money and crowned as the
       victors of MegaMinerAI.
     </p>
+    <hr />
+    <hr class="divide" />
   </div>
 </template>
 
@@ -33,4 +36,13 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+hr.divide {
+  border-top: 2px solid #1b3e50;
+  border-radius: 5px;
+}
+
+hr {
+  border: 1px solid #fff;
+}
+</style>
