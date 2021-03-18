e2f69f4b052735e6a79db47791d5b381f065651e
TheHydra778 <54971206+TheHydra778@users.noreply.github.com>
2021-03-18 16:02:08 -0500

    Linked the sponsor page to the main page

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
       </router-link> |
       <router-link to="/about">
         About
+      </router-link> |
+      <router-link to="/sponsor">
+        Sponsor
       </router-link>
     </div>
     <LoadIcon v-if="isLoading" />
