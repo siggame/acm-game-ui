76b287b0ef73644a8511c5dc741497ce0277bf4a
Nick_Latinette <nicholas.latinette@gmail.com>
2021-05-01 11:19:37 -0500

    setup simple dynamic fonts for about

--- src/components/AboutFolder/Jumbotron.vue
+++ src/components/AboutFolder/Jumbotron.vue
@@ -3,14 +3,20 @@
     class="jumbotron bg-cover"
     style="background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url(https://placeimg.com/1000/480/nature)"
   >
-    <h1 class="display-4">
+    <h1
+      class="display-4"
+      style="font-size: 2vw;"
+    >
       ACM-GAME
     </h1>
-    <p class="lead">
+    <p
+      class="lead"
+      style="font-size: 1.3vw;"
+    >
       We Code the Game.
     </p>
     <hr class="my-4">
-    <p>
+    <p style="font-size: 1.3vw;">
       You Code the Player.
     </p>
     <!---<a class="btn btn-primary btn-lg" href="https://www.google.com" role="button">Learn more</a>--->
