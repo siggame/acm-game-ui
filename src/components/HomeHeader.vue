9e926d535fe542fa30e1ec68fd19817f77fad3e0
Drew <drewbrissette@gmail.com>
2021-07-31 12:59:15 -0500

    work on jumbotron

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,16 +1,15 @@
 <template>
-  <div
-    class="home-header"
-  >
-    <div class="wrench-picture">
+  <div class="home-header">
+    <div class="slogo-container">
       <img
         class="wrench-svg"
         :src="getImg(`wrench.svg`)"
       >
-    </div>
-    <div class="acm-text">
-      <h1>ACM GAME</h1>
-      <p>we code the game, you code the player</p>
+      <div class="acm-text">
+        <h1>ACM GAME</h1>
+        <h3>WE CODE THE GAME</h3>
+        <h3>YOU CODE THE PLAYER</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -25,21 +24,31 @@ export default Vue.extend({
     getImg,
   },
 });
-
 </script>
 
 <style scoped>
-    .home-header {
-        background-image: url("../assets/games.gif");
-        width: 100%;
-        height: 800px;
-    }
-    .wrench-picture {
-        width: 40 px;
-        height: 40 px;
-    }
-    .wrench-svg {
-        width: 40 px;
-        height: 40 px;
-    }
+.home-header {
+  justify-content: center;
+  position: relative;
+  background-image: url("../assets/games.gif");
+  height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.slogo-container {
+  display: flex;
+  position: relative;
+}
+.wrench-svg {
+  width: 400px;
+  height: 400px;
+}
+.acm-text h1 {
+  font-size: 500%;
+}
+.acm-text h3 {
+  font-size: 200%;
+}
 </style>
