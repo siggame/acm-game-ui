9bedc57041879c9c23c4fb307bb73a6d295543d2
Jonathan Henderson <jkhenderson999@gmail.com>
2021-08-03 19:37:18 -0500

    Update contact info links

--- src/services/common.ts
+++ src/services/common.ts
@@ -2,6 +2,22 @@ export function getImg(path: string) {
   return require(`@/assets/${path}`);
 }
 
-export default {
-  getImg,
-};
+/// Valid social media sites.
+const VALID_SOCIALS = [
+  'email',
+  'linkedin',
+  'github',
+  'discord',
+  'steam',
+];
+
+export function transformSocialLink(site: string, link: string): string {
+  switch (site) {
+    case 'email': {
+      return `mailto:${link}`;
+    }
+    default: {
+      return 'http://';
+    }
+  }
+}
